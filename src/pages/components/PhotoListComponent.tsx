cf0231b86e5e6957867a6008365d3b35f0c794b5
Villiyrtti <laura.janttonenoutlook.com>
2026-03-12 21:12:20 +0200

    Add a button to load more photos

--- src/pages/components/PhotoListComponent.tsx
+++ src/pages/components/PhotoListComponent.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Grid } from "@mui/material"
+import { Button, Grid } from "@mui/material"
 import { getPhotos } from '../../api/API';
 
 import PhotoCardComponent from '../components/PhotoCardComponent';
@@ -7,16 +7,24 @@ import type { Photo } from '../../types/CommonTypes';
 
 const PhotoListComponent = () => {
     const [photos, setPhotos] = useState<Photo[]>([]);
-    const pageNumber = 1;
+    const [pageNumber, setPageNumber] = useState<number>(1);
 
-    const updatePhotoArray = async () => {
-        const fetchedPhotos = await getPhotos(pageNumber);
-        setPhotos(fetchedPhotos);
+    const fetchPhotos = async(pageNumber: number) => {
+        return await getPhotos(pageNumber);
+    };
+
+    const fetchMorePhotos = async() => {
+        const newPageNumber = pageNumber +1
+        setPageNumber(newPageNumber);
+        const fetchedPhotos = await fetchPhotos(newPageNumber);
+        const newList: Photo[] = [...photos, ...fetchedPhotos];
+        setPhotos(newList);
     };
 
     useEffect(() => {
         const fetchData = async() => {
-            return updatePhotoArray();
+            const fetchedPhotos = await fetchPhotos(pageNumber);
+            setPhotos(fetchedPhotos);
         };
 
         if (!photos.length) {
@@ -24,27 +32,27 @@ const PhotoListComponent = () => {
         };
     }, []);
 
-  // This hook can be utilized when button to bottom of page is implemented
-  // useEffect(() => {
-  //     const fetchData = async() => {
-  //         return updatePhotoArray();
-  //     }
-  //     fetchData();
-  // }, [pageNumber]);
     return(
         <Grid 
-          container 
-          size={9} 
-          columns={{ 
-            xs: 4, 
-            sm: 8, 
-            md: 12 }}>
+            container 
+            size={9} 
+            columns={{ 
+                xs: 4, 
+                sm: 8, 
+                md: 12 }}
+            rowSpacing={1}
+            columnSpacing={1}>
             { photos.length > 0 && photos.map((photo: Photo) => {
               return (
                 <Grid key={photo.id} size={{ xs: 12, md: 4, lg: 3 }}>
                   <PhotoCardComponent item={photo} />
                 </Grid> )
             })}
+            <Grid size={12}>
+                <Button onClick={() => fetchMorePhotos()}>
+                    Load More
+                </Button>
+            </Grid>
         </Grid>
     );
 };
